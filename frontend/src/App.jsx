6b7d13b7f3c6cbd3989b68d61e53efb7e810fdc8
Shubha274 <pandeyshubha27@gmail.com>
2025-03-01 12:28:39 +0530

    added new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,8 @@ import Attendance from "./Components/Attendance_student/Attendance";
 import CourseEnrolled from "./Components/CourseEnrolled/CourseEnrolled";
 import ResultChart from "./Components/Charts/ResultChart";
 import Attendence1 from "./scenes/Attendence1/Attendence1";
+// import Calender from "./scenes/calender/Calender";
+
 import { useSelector } from "react-redux";
 import ProtectedRoute from "./Components/ProtectedRoute";
 
@@ -61,6 +63,7 @@ function App() {
               <Route path="/assignment" element={<Assignment />} />
               <Route path="/timetable" element={<TimeTable />} />
               <Route path="/attendance" element={<Attendence1 />} />
+              {/* <Route path="/calendar" element={<Calender />} /> */}
               <Route path="/course-enrolled" element={<CourseEnrolled />} />
               <Route path="/resultChart" element={<ResultChart />} />
             </Route>
