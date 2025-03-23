7845664accc8868281d8aac55cea9b29d8e015bf
Pragati-3003 <jpragati3003@gmail.com>
2025-03-23 22:09:16 +0530

    Admin Pages Configured

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -38,8 +38,10 @@ import TeacherManagement from "./Admin/TeacherManagement/TeacherManagement";
 import AddTeacherCourse from "./Admin/AddTeacherCourse/AddTeacherCourse"
 import CourseManagement from "./Admin/CourseManagement/CourseManagement"
 import AdminProfile from "./Admin/AdminProfile/AdminProfile";
+import AddSemesterResult from "./Admin/AddSemesterResult/AddSemesterResult";
 import AttendanceReport from "./Components/Attendance_student/Attendance";
-
+import UploadStudentTimetable from "./Admin/UploadStudentTimetable/UploadStudentTimetable";
+import UploadTeacherTimetable from "./Admin/UploadTeacherTimetable/UploadTeacherTimetable";
 function App() {
   const token = localStorage.getItem("token");
   let role = null;
@@ -131,6 +133,9 @@ function App() {
             <Route path="/admin/addteachercourse" element={<AddTeacherCourse />} />
             <Route path="/admin/coursemanagment" element={<CourseManagement />} />
             <Route path="/admin/profile" element={<AdminProfile/>} />
+            <Route path="/admin/resultmgmt" element={<AddSemesterResult/>} />
+            <Route path="/admin/addStudentTimetable" element={<UploadStudentTimetable/>} />
+            <Route path="/admin/addTeacherTimetable" element={<UploadTeacherTimetable/>} />
            
             {/* Fallback route for invalid paths */}
 
